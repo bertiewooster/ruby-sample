39364f1d248530df005b2a2b0df4304689556708
bertiewooster <jeremy@whitegloveapps.com>
2014-03-03 21:39:33 -0500

    Added some postgres to environments.rb

---
+++ environments.rb
@@ -0,0 +1,16 @@
+configure :development do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://jemonat@localhost/elements')
+end
+
+configure :production do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/elements')
+end
+
+ActiveRecord::Base.establish_connection(
+	:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+	:host     => db.host,
+	:username => db.user,
+	:password => db.password,
+	:database => db.path[1..-1],
+	:encoding => 'utf8'
+)
--- web.rb
+++ web.rb
@@ -1,5 +1,20 @@
 require 'sinatra' #comment
+require 'sinatra/activerecord'
+require 'active_record'
+require './environments'
+
+class Element < ActiveRecord::Base
+end
+
+class Base < ActiveRecord::Base
+end
+
+class Orb < ActiveRecord::Base
+end
+
+class Orbital < ActiveRecord::Base
+end
 
 get '/' do
-  "Hello, minna-san"
+  "Hello, minna-san!"
 end
